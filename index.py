7e97c856be20b5ff8dda3032695e066c3e547846
pasj52 <78424331+pasj52@users.noreply.github.com>
2023-06-15 17:05:57 +0200

    Update index.py

--- index.py
+++ index.py
@@ -7,22 +7,6 @@ from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 # Define a simple Streamlit app
 st.title("Ask the DECTRIS digital assistant!")
-
-# If the 'Load' button is clicked
-if st.button("Load"):
-        try:
-            # Configure prompt parameters and initialise helper
-            max_input_size = 4096
-            num_output = 256
-            max_chunk_overlap = 20
-                
-            # Load documents from the 'data' directory
-            documents = SimpleDirectoryReader('data_development').load_data()
-            index = VectorStoreIndex.from_documents(documents)
-            index.storage_context.persist()
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-
 query = st.text_input("What would you like to ask about our detectors?", "")
 
 # If the 'Submit' button is clicked
@@ -31,6 +15,14 @@ if st.button("Submit"):
         st.error(f"Please provide the search query.")
     else:
         try:
+            # Configure prompt parameters and initialise helper
+            max_input_size = 4096
+            num_output = 256
+            max_chunk_overlap = 20
+                
+            # Load documents from the 'data' directory
+            documents = SimpleDirectoryReader('data_development').load_data()
+            index = VectorStoreIndex.from_documents(documents) 
             query_engine = index.as_query_engine()
             response = query_engine.query(query).response
             st.success(response)
